d73801f44b8aa4e3f333bea4e1543b15da3d263b
Diomedes Git <diomedesgit@gmail.com>
2024-07-14 05:08:20 +0100

    fiddlin

---
+++ letter_generators_and_iterators/letter_reader.py
@@ -0,0 +1,13 @@
+import ollama
+import os
+import json
+from datetime import datetime
+
+time_stamp = datetime.now().strftime("%Y%m%d_%H%M")
+
+
+primary_model = 'llama3'
+secondary_model = 'llama3'
+tertiary_model = 'llama3'
+analysis_model = 'llama3'
+
