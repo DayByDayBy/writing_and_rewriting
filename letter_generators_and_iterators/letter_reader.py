9b7ee5470f1dbfc916a93f468cbacd88717ee6de
Diomedes Git <diomedesgit@gmail.com>
2024-07-30 20:00:52 +0100

    forgot to commit last time, no massive changes

--- letter_generators_and_iterators/letter_reader.py
+++ letter_generators_and_iterators/letter_reader.py
@@ -1,13 +1,25 @@
-import ollama
-import os
-import json
-from datetime import datetime
+from pathlib import Path
 
-time_stamp = datetime.now().strftime("%Y%m%d_%H%M")
+# import ollama
+# import os
+# import json
+# from datetime import datetime
+
+# time_stamp = datetime.now().strftime("%Y%m%d_%H%M")
 
 
-primary_model = 'llama3'
-secondary_model = 'llama3'
-tertiary_model = 'llama3'
-analysis_model = 'llama3'
+# primary_model = 'llama3'
+# secondary_model = 'llama3'
+# tertiary_model = 'llama3'
+# analysis_model = 'llama3'
+
+
+
+directory = Path('/letters')
+
+for txt_file in directory.glob('*.txt'):
+    with open(txt_file, 'r') as file:
+        content = file.read()
+        print(content)
+        
 
